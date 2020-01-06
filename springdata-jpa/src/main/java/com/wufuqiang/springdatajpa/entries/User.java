8a6127aa7bf66f8396f29fbc0acc00500a097e77
wufq <wufq@auto_create>
2020-01-06 15:12:57 +0800

    init

--- springdata-jpa/src/main/java/com/wufuqiang/springdatajpa/entries/User.java
+++ springdata-jpa/src/main/java/com/wufuqiang/springdatajpa/entries/User.java
@@ -1,7 +1,6 @@
 package com.wufuqiang.springdatajpa.entries;
 
 import lombok.Data;
-import lombok.Setter;
 import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.Column;
@@ -10,8 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@DynamicInsert
 @Table(name="AUTH_USERS")
+@Data
 public class User {
     @Id
     private Long id;
@@ -21,36 +20,4 @@ public class User {
     private String account;
     @Column(length = 64)
     private String pwd;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getPwd() {
-        return pwd;
-    }
-
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
-    }
 }
